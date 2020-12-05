207b61b2944a10219a89cabcf12263806b8d40dc
Darren Matis <darren.matis@sap.com>
2020-12-05 12:53:22 -0800

    Added About page

--- gatsby-config.js
+++ gatsby-config.js
@@ -29,9 +29,8 @@ module.exports = {
       options: {
         apiURL: `http://localhost:1337`,
         queryLimit: 1000, // Default to 100
-        // contentTypes: [`jobs`, `projects`, `blogs`, `abouts`],
         contentTypes: [`jobs`, `projects`, `blogs`],
-        singleTypes: [],
+        singleTypes: [`about`],
       },
     },
     // {
