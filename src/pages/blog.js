d4486ecfd3677032c01f87bc4fc433ebe2e09126
Darren Matis <darren.matis@sap.com>
2020-12-26 14:30:02 -0800

    Add recaptcha to the contact form

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -2,7 +2,6 @@ import React from "react"
 import Layout from "../components/Layout"
 import { graphql } from "gatsby"
 import Blogs from "../components/Blogs"
-// ...GatsbyImageSharpFluid
 
 const Blog = ({data}) => {
   const {
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,23 +1,43 @@
 import React from "react"
 import Layout from "../components/Layout"
+import { Helmet } from "react-helmet"
+import { graphql } from "gatsby"
 
-const contact = () => {
+export const query = graphql`
+  {
+    site {
+      siteMetadata {
+        siteTitle: title
+      }
+    }
+  }
+`
+
+const contact = ({data}) => {
+  const { siteTitle } = data.site.siteMetadata;
+
   return (
-    <Layout>
-      <section className="contact-page">
-        <article className="contact-form">
-          <h3>Get in touch</h3>
-          <form action="https://formspree.io/f/mpzoyape" method="POST">
-            <div className="form-group">
-              <input type="text" name="name" placeholder="name" className="form-control" required />
-              <input type="email" name="email" placeholder="email" className="form-control" required />
-              <textarea name="message" id="" rows="10" placeholder="message" className="form-control" required></textarea>
-            </div>
-            <button type="submit" className="submit-btn btn">Submit</button>
-          </form>
-        </article>
-      </section>
-    </Layout>
+    <>
+      <Helmet htmlAttributes={{lang: "en" }} title={`Contact | ${siteTitle}`}>
+        <script src="https://www.google.com/recaptcha/api.js"></script>
+      </Helmet>
+      <Layout>
+        <section className="contact-page">
+          <article className="contact-form">
+            <h3>Get in touch</h3>
+            <form action="https://formspree.io/f/mpzoyape" method="POST">
+              <div className="form-group">
+                <input type="text" name="name" placeholder="name" className="form-control" required />
+                <input type="email" name="email" placeholder="email" className="form-control" required />
+                <textarea name="message" id="" rows="10" placeholder="message" className="form-control" required></textarea>
+                <div className="g-recaptcha" data-sitekey="6Lfh6BUaAAAAAEe8d43cfrF5IoDHYqsZelUdpphp"></div>
+              </div>
+              <button type="submit" className="submit-btn btn">Submit</button>
+            </form>
+          </article>
+        </section>
+      </Layout>
+    </>
   )
 }
 
